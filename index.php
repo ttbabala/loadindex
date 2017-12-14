253aaa1a639cd93b88cca20642267e49bf237d86
ttbabala <hqiotzx@126.com>
2017-12-14 18:15:23 +0800

    first submit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
+  <meta charset="utf-8" /> 
+  <title>宁夏大学新闻传播学院学生创新实践平台</title> 
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="author" content="qianxunteam" /> 
+  <link rel="stylesheet" href="css/style.css" />
+  <!--[if IE 6]>		<![endif]--> 
+ </head> 
+ <body> 
+         <div id="logo"><img src="img/logo.png" /></div>
+         <div id="logo-text"><img src="img/logo-text.png" /></div>
+         <div id="callboard"> 
+               <ul>                     
+                    <li> 
+                        <span style="color: moccasin"><img alt="" src="img/notice-icon.png" width="15px" height="15px"/><a href="javascript:;" class="pop-notice" value="" title="" content="" style="margin-left:10px;padding-top:10px;">“自适应网页设计”到底是怎么做到的？其实并不难。</a></span> 
+                    </li> 
+                    <li> 
+                        <span style="color: moccasin"><img alt="" src="img/notice-icon.png" width="15px" height="15px"/><a href="javascript:;" class="pop-notice" value="" title="" content="" style="margin-left:10px;padding-top:10px;">“自适应网页设计”到底是怎么做到的？其实并不难。</a></span> 
+                    </li> 
+                    <li> 
+                        <span style="color: moccasin"><img alt="" src="img/notice-icon.png" width="15px" height="15px"/><a href="javascript:;" class="pop-notice" value="" title="" content="" style="margin-left:10px;padding-top:10px;">“自适应网页设计”到底是怎么做到的？其实并不难。</a></span> 
+                    </li> 
+
+               </ul> 
+         </div> 
+         <div id="button-download">
+                <a href="javascript:;" target="_blank" class="i-bbt-left" >关于我们</a> 
+                <a href="javascript:;" target="_blank" class="i-bbt-right" >联系我们</a> 
+         </div>
+         <div id="nav"> 
+                <ul> 
+                 <li> <a href="http://localhost/nxipp/zysx/" target="_blank" id="link-about"> <span class="link-text-wrapper"> <span class="link-text"> <em class="link-icon"></em>专业实训 </span> </span> </a> </li> 
+                 <li> <a href="http://localhost/nxipp/yxzy/" target="_blank" id="link-log"> <span class="link-text-wrapper"> <span class="link-text"> <em class="link-icon"></em>优秀作业 </span> </span> </a> </li> 
+                 <li> <a href="http://localhost/nxipp/cxcy/" target="_blank" id="link-guide"> <span class="link-text-wrapper"> <span class="link-text"> <em class="link-icon"></em>创新创业 </span> </span> </a> </li> 
+                 <li> <a href="http://localhost/nxipp/yczp/" id="link-app" target="_blank"> <span class="link-text-wrapper"> <span class="link-text"> <em class="link-icon"></em>学科竞赛 </span> </span> </a> </li> 
+                 <li> <a href="http://localhost/nxipp/gygg/" id="link-comment" target="_blank"> <span class="link-text-wrapper"> <span class="link-text"> <em class="link-icon"></em>公益广告 </span> </span> </a> </li> 
+                </ul> 
+                <div id="nav-overlay"></div> 
+         </div> 
+ </body>
+</html>
